24c3c33495750d95bfd55ef0b9cd3997596966ff
Kristen Peck <kpeck71@gmail.com>
2017-11-24 18:15:56 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,9 +4,10 @@ class Student
   attr_accessor :id, :name, :grade
 
   def initialize(id:nil,name,grade)
+    @id = id
     @name = name
     @grade = grade
-    @id = id
+
   end
 
   def self.create_table
